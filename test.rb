73984ae391c6ce5dbb061eae3f1c78370a4c8be7
JJ Asghar <jj@getchef.com>
2014-07-25 15:36:03 -0500

    working with chef-metal 0.13 bulit from master

--- test.rb
+++ test.rb
@@ -1,15 +1,29 @@
 require 'chef_metal'
 
-# with_driver 'vagrant'
 
-# with_machine_options :vagrant_options => { 'vm.box' => 'centos65' },
-#                      :vagrant_config =>
-#       "config.vm.network forwarded_port, guest: 443, host: 9443"
-#       "config.vm.network forwarded_port, guest: 8773, host: 9773"
-#       "config.vm.network forwarded_port, guest: 8774, host: 9774"
-#       "config.vm.network forwarded_port, guest: 4001, host: 4001"
+mario_config = <<-ENDCONFIG
+  config.vm.box = "precise64"
+  config.vm.network "forwarded_port", guest: 443, host: 9443
+  config.vm.network "forwarded_port", guest: 4002, host: 4002
+  config.vm.network "forwarded_port", guest: 5000, host: 5000
+  config.vm.network "forwarded_port", guest: 8773, host: 8773
+  config.vm.network "forwarded_port", guest: 8774, host: 8774
+  config.vm.network "forwarded_port", guest: 35357, host: 35357
+ENDCONFIG
+
+# luigi_config = <<-ENDCONFIG
+#   config.vm.box = "precise64"
+#   config.vm.network "forwarded_port", guest: 459, host: 9488
+# ENDCONFIG
 
 machine 'mario' do
-    tag 'itsa_me'
-    converge true
+  tag 'itsa_me'
+  machine_options :vagrant_config => mario_config
+  converge true
 end
+
+# machine 'luigi' do
+#   tag 'itsa_me'
+#   machine_options :vagrant_config => luigi_config
+#   converge true
+# end
