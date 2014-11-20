098989dda716e0b7cde54d18e5e7436237ed75e6
Seth Thomas <sthomas@getchef.com>
2014-11-20 14:50:53 -0800

    Abstract box name and use add_machine_options, use machine_batch in multi-neutron, and remove (non-working) ubuntu boxes

--- vagrant_linux.rb
+++ vagrant_linux.rb
@@ -4,10 +4,7 @@ vagrant_box 'centos65' do
   url 'http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_centos-6.5_chef-provisionerless.box'
 end
 
-vagrant_box 'precise64' do
-  url 'http://files.vagrantup.com/precise64.box'
-end
-
-vagrant_box 'trusty64' do
-  url 'http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box'
-end
+with_driver "vagrant:#{File.dirname(__FILE__)}/vms"
+with_machine_options :vagrant_options => {
+  'vm.box' => 'centos65'
+}
