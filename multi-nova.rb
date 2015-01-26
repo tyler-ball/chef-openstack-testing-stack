9d7f8080f513d43ccb55aae7813be25db99dfe66
JJ Asghar <jj@getchef.com>
2015-01-26 15:53:03 -0600

    Updated some networking issues for AIO-nova

--- multi-nova.rb
+++ multi-nova.rb
@@ -4,6 +4,7 @@ controller_config = <<-ENDCONFIG
   config.vm.network "forwarded_port", guest: 443, host: 9443 # dashboard-ssl
   config.vm.network "forwarded_port", guest: 4002, host: 4002
   config.vm.network "forwarded_port", guest: 5000, host: 5000
+  config.vm.network "forwarded_port", guest: 6080, host: 6080
   config.vm.network "forwarded_port", guest: 8773, host: 8773 # compute-ec2-api
   config.vm.network "forwarded_port", guest: 8774, host: 8774 # compute-api
   config.vm.network "forwarded_port", guest: 35357, host: 35357
