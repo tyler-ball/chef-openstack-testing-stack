3d5ec7dbeac5c7e9b32db7af63da42cb21dda0f4
Mark Vanderwiel <vanderwl@us.ibm.com>
2015-02-02 13:50:52 -0600

    Update vagrant_linux.rb

--- vagrant_linux.rb
+++ vagrant_linux.rb
@@ -4,11 +4,6 @@ vagrant_box 'centos7' do
   url 'http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_centos-7_chef-provisionerless.box'
 end
 
-vagrant_box 'centos65' do
-  url 'http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_centos-6.5_chef-provisionerless.box'
-end
-
-
 vagrant_box 'ubuntu14' do
   url 'http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_ubuntu-14.04_chef-provisionerless.box'
 end
