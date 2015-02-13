e264b00def233038c32ffe4b93c330feec263710
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2015-02-13 14:25:44 +0100

    Fix indent in destroy_all.rb

--- destroy_all.rb
+++ destroy_all.rb
@@ -1,6 +1,6 @@
 require 'chef/provisioning'
 
 machine_batch do
-    machines search(:node, '*:*').map { |n| n.name }
-      action :destroy
+  machines search(:node, '*:*').map { |n| n.name }
+  action :destroy
 end
