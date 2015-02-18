647c2ccaa2e79c936656da6122d6d987e4a92385
tyler-ball <tyleraball@gmail.com>
2015-02-17 16:55:32 -0800

    Setting default knife secret so it doesn't have to be passed all the time

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -12,3 +12,4 @@ chef_server_url 'https://api.opscode.com/organizations/my_awesome_org'
 cache_type 'BasicFile'
 cache_options(path: "#{ENV['HOME']}/.chef/checksums")
 cookbook_path ["#{current_dir}/../cookbooks"]
+knife[:secret_file] = "#{current_dir}/encrypted_data_bag_secret"
